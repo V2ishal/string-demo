e18e03bd4a8d2ccf48277cfb9d0b4a47778564f0
V2ishal <vishalmhaske928@gmail.com>
2026-06-09 14:07:34 +0530

    branch main file app .js empty

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-//Q2 string is palindrome or not 
-
-let student="nayan";
-let palindrome="string is palindrome";
-
-for(let i=0; i<student.lenght/2; i++){
-   if(str[i]!==str[str.length-1-i]){
-    palindrome="string is not a palindrome";
-    break;
-   }
-}
-console.log(palindrome);
 
