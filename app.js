1815a57bd47a728715d4b42a293a68df5c8ba48a
V2ishal <vishalmhaske928@gmail.com>
2026-06-09 14:32:02 +0530

    string is palindrome solve using method

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+//q2 string is palindrome or not using method
+let student="nayan";
+let reverse=student.split("").reverse().join("");
+if(student===reverse){
+    console.log("string is palindrome");
+}
+else{
+    console.log("string is not palindrome");
+}
