13347e7b56e4f1d47e5e3569f0e64a75b83353c4
V2ishal <vishalmhaske928@gmail.com>
2026-06-09 15:57:39 +0530

    index .html empty

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-//q2 string is palindrome or not using method
-let student="kiran";
-let reverse=student.split("").reverse().join("");
-if(student===reverse){
-    console.log("string is palindrome");
-}
-else{
-    console.log("string is not palindrome");
-}
+
