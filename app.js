6f4362e69e9f0c6b76f6772119fedd7056f30508
V2ishal <vishalmhaske928@gmail.com>
2026-06-11 21:02:45 +0530

    string is palindrome using method solve

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let student="rahul";
 let reverse=student.split("").reverse().join("");
-console.log(reverse);
+ if(student==reverse){
+    console.log("string is palindrome");
+ }
+ else{
+    console.log("string is not a palindrome");
+ }
