3abfb11e0f5db832e074b6971c6f084341629544
V2ishal <vishalmhaske928@gmail.com>
2026-06-12 11:09:27 +0530

    html css empty

--- app.js
+++
@@ -1,8 +0,0 @@
-let student="rahul";
-let reverse=student.split("").reverse().join("");
- if(student==reverse){
-    console.log("string is palindrome");
- }
- else{
-    console.log("string is not a palindrome");
- }
