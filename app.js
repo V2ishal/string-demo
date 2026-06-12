79fe13c60db343dd0fa52a4e12c9aae009aed038
V2ishal <vishalmhaske928@gmail.com>
2026-06-12 11:12:34 +0530

    app.js and index.html empty

--- app.js
+++
@@ -1,8 +0,0 @@
-let str="rahul";
-let reverse=str.split("").reverse().join("");
-if(str===reverse){
-    console.log("string is palindrome");
-}
-else{
-    console.log("string is not a palindrome");
-}
